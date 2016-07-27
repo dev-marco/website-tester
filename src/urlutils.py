b1e80ef5f5650a86bc7130baaa46d79924ea8726
Marco <marcoaremail@gmail.com>
2016-07-27 03:31:30 -0300

    Changed rule keyword format to cover more attributes

--- src/urlutils.py
+++ src/urlutils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import os, re, sys, socket, unicodedata
+import os, re, sys, socket, unicodedata, collections
 import http.client
 import urllib.robotparser
 import itertools as it
@@ -104,12 +104,32 @@ normalize_filename.non_word = re.compile(r'[^\w_.,-]')
 def normalize_filepath (name):
     return os.path.normpath('/'.join(normalize_filename(p) for p in name.split('/')))
 
-def compile_rule (rule, urls = []):
-    if '[%url%]' in rule:
-        return [ re.compile(rule.replace('[%url%]', re.escape(url.netloc))) for url in urls ]
-    return [ re.compile(rule) ]
+class RuleDict (dict):
+    def __missing__ (self, key):
+        return '[*' + key + '*]'
 
-def read_rules (name, urls = []):
+def compile_rules (rules, urls):
+    result = collections.OrderedDict()
+
+    url_dicts = list(RuleDict({ str(key): re.escape(str(value)) for key, value in url.asdict().items() }) for url in urls)
+
+    for rule in rules:
+        escaped_rule = compile_rules.format.sub(r'{\1}', compile_rules.escape.sub(r'\1\1', rule))
+
+        for url_dict in url_dicts:
+            resulting_rule = escaped_rule.format_map(url_dict)
+
+            result[resulting_rule] = None
+
+            if resulting_rule == rule:
+                break
+
+    return collections.OrderedDict.fromkeys(map(re.compile, result.keys()))
+
+compile_rules.format = re.compile(r'\[\*([a-z_]+)\*\]')
+compile_rules.escape = re.compile(r'([{}])')
+
+def read_rules (name, urls):
 
     rules = []
 
@@ -117,6 +137,6 @@ def read_rules (name, urls = []):
         for line in rules_file:
             line = line.rstrip('\n\r')
             if line[0] != '#':
-                rules.extend(compile_rule(line, urls))
+                rules.append(line)
 
-    return rules
+    return compile_rules(rules, urls)
